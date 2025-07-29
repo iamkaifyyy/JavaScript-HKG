945a6b93abe237a4fcd154cd3c202f6a752718fe
kaifyyy <mkaifm728@gmail.com>
2025-07-30 03:07:14 +0530

    changed the fileName

--- Basics.js
+++
@@ -1,5 +0,0 @@
-const accountID = 144533;
-let accountEmail = "kaifyyy@google.com";
-var accountPassword = "12345"
-
-accountCity = "Lucknow";
---
+++ basic.js
@@ -0,0 +1,13 @@
+const accountID = 144533;
+let accountEmail = "kaifyyy@google.com";
+var accountPassword = "12345"
+
+accountCity = "Lucknow";
+
+
+accountEmail = "boobs@google,com";
+accountPassword = "2121212";
+
+accountCity = "Bangalore"
+
+console.table({accountEmail, accountID, accountPassword, accountCity});
