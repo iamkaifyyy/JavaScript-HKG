39955eb3425957eb700481d56f8ec3a58ee5bc86
kaifyyy <mkaifm728@gmail.com>
2025-07-30 18:27:23 +0530

    Changes here

--- basic.js
+++ basic.js
@@ -1,13 +1,37 @@
 const accountID = 144533;
 let accountEmail = "kaifyyy@google.com";
-var accountPassword = "12345"
+var accountPassword = "12345";
 
 accountCity = "Lucknow";
-
 
 accountEmail = "boobs@google,com";
 accountPassword = "2121212";
 
-accountCity = "Bangalore"
+accountCity = "Bangalore";
 
-console.table({accountEmail, accountID, accountPassword, accountCity});
+console.table({ accountEmail, accountID, accountPassword, accountCity });
+
+
+
+
+// Data Types in JavaScript
+
+/*
+
+Note: "use strict" // treats all code as newer Updated Version 
+
+Note: alert ( 2 + 3) // we re uisng node.js not V8 Engine Browser
+
+ */
+
+// Note: we are here using Data types and ECMA Standards.
+
+
+// code Readbility will be high 
+
+
+let name = "kaifyyy";
+let age = 20;
+let isLoggedIn = false;
+
+
