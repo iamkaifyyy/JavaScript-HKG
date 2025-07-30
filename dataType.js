f1420a49121a1d6e0def2e92e4c7d5d988dac4e3
kaifyyy <mkaifm728@gmail.com>
2025-07-31 00:07:09 +0530

    committing

--- dataType.js
+++ dataType.js
@@ -1,11 +1,26 @@
-let score = "33abc";
-console.log(typeof score);
-console.log(typeof(score));
+// let score = "33abc";
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 
-let valueInNumber = Number (score)
-console.log(typeof score);
+// let valueInNumber = Number (score)
+// console.log(typeof score);
 
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
