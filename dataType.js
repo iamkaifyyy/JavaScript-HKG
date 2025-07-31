cb402e14d0b415af2223704dda2790596903a52c
kaifyyy <mkaifm728@gmail.com>
2025-07-31 13:13:30 +0530

    created new File Operations.js

--- dataType.js
+++ dataType.js
@@ -12,8 +12,15 @@
 
 // console.log(booleanIsLoggedIn);
 
+
+
+
+/*
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+*/
+
