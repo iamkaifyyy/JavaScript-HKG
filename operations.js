4638e513bdbba42f5905de00b13f248a6a40546e
kaifyyy <mkaifm728@gmail.com>
2025-07-31 13:28:00 +0530

    operations.js

---
+++ operations.js
@@ -0,0 +1,14 @@
+// Operations in JS
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+
+console.log( 2+2);
+console.log(2-2);
+console.log(2**2);
+console.log();
+console.log();
+console.log();
