7716853881e5cd2ae1eaf12ede0187608614ff94
kaifyyy <mkaifm728@gmail.com>
2025-07-31 13:56:35 +0530

    commit

--- operations.js
+++ operations.js
@@ -5,10 +5,26 @@ let negValue = -value;
 
 console.log(negValue);
 
-
+// basic maths operations
 console.log( 2+2);
 console.log(2-2);
 console.log(2**2);
-console.log();
-console.log();
-console.log();
+console.log( 2/2);
+console.log(2/3);
+console.log(2%3);
+
+
+// Strings in JS
+
+let str1 = "hello";
+let str2 = "kaifyyy";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+// some exceptions
+
+console.log("1" + 2 + 2); // gives 122
+console.log(1 + 2 + "2"); // gives 32
+
