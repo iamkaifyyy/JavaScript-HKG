46bcd7c6676199bade02c7866ef6eb55638d4e1e
kaifyyy <mkaifm728@gmail.com>
2025-08-01 19:02:55 +0530

    committing once more

--- strings.js
+++ strings.js
@@ -3,4 +3,9 @@ const repoCount = 50;
 
 console.log(name + repoCount + "Value");
 
+// infact of doing that we can do this 
+
+console.log(`hello Im ${name} and my repoCount is ${repoCount}`);
+
+
 
