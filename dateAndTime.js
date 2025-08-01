354c9a367865d444c20965249fcead002d97d395
kaifyyy <mkaifm728@gmail.com>
2025-08-02 02:22:42 +0530

    new Chnages

---
+++ dateAndTime.js
@@ -0,0 +1,21 @@
+const score = 400;
+const boobs = new Number(score)
+
+console.log(boobs);
+
+
+
+console.log(score);
+
+
+
+
+const balance = new Number(100)
+console.log(balance);
+
+
+// date and Time Experiments here
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
--- operations.js
+++ operations.js
@@ -36,3 +36,6 @@ gameCounter--;
 console.log(gameCounter);
 
 
+
+
+
