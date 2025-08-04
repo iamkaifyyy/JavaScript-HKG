b5fe426a493b6892dc5b1acb4e8679cf7ed48d5f
kaifyyy <mkaifm728@gmail.com>
2025-08-04 18:19:55 +0530

    commit once more

--- array.js
+++ array.js
@@ -7,9 +7,14 @@ const myArr = [0, 1, 2, 3, 4, 5, true, "kaifyyy"];
 console.log(myArr[0]);
 console.log(myArr[4]);
 
+const myHeroes = ["batman", "ironman"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+console.log(`array is ${myArr2}`);
 
 
+// Array Methods
 
+myArr.push(6, 2);
+console.log(myArr);
 
-
-// 
