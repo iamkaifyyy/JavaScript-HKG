6bb54755cecad14bcf9d656dcd2bc772fdde28a0
kaifyyy <mkaifm728@gmail.com>
2025-08-05 00:41:24 +0530

    more changes on the way!

---
+++ objects.js
@@ -0,0 +1,14 @@
+// learning about Objects
+
+
+function Person(name, age, city) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+
+}
+
+
+const kaif = new Person("kaifuu", 17, "lucknow");
+const john = new PE=E
+
