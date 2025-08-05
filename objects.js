b9c696d2588cd78bf8474fcfca1834f78bafb679
kaifyyy <mkaifm728@gmail.com>
2025-08-05 21:12:33 +0530

    more changes in objects part 2

--- objects.js
+++ objects.js
@@ -1,67 +1,127 @@
-// // learning about Objects
+// // // learning about Objects
 
 
-// // function Person(name, age, city) {
-// //     this.name = name;
-// //     this.age = age;
-// //     this.city = city;
+// // // function Person(name, age, city) {
+// // //     this.name = name;
+// // //     this.age = age;
+// // //     this.city = city;
 
+// // // }
+
+
+// // // singleton - we can generate the constructor objects
+ 
+// // const mySym = Symbol("key1")
+
+// // const jsuser = {
+// //     name: "kaifuuu",
+// //     "fullname": "mohdkaifuuuu",
+// //     [mySym]: "mykey1",
+// //     age: 20,
+// //     city: "Lucknow",
+// //     email: "kaifuu@google.com",
+// //     isLoggedIn: false,
+// //     lastLoginDays: ["Monday", "Saturday"]
+// // }
+ 
+
+// // console.log(jsuser);
+// // console.log(jsuser.email);
+// // console.log("diff approach");
+// // console.log(jsuser["email"]);
+
+
+// // console.log("checking the Symbol works here or not!");
+
+// // console.log([mySym]);
+// // console.log(typeof [mySym]);
+
+// //  jsuser.greeting = function(){
+// //     console.log("hello jsuser boobs");
+// //  }
+
+// //  jsuser.greetingTwo= function(){
+// //     console.log(`hello jsuser ,${this.name}`);
+// //  }
+
+
+// //  console.log(jsuser.greeting());
+// //   console.log(jsuser.greetingTwo());
+
+
+
+// // Objects Part 2
+
+// const tinderUser = new Object() 
+
+
+// tinderUser.id = "123abc"
+// tinderUser.name = "kaifuuu"
+// tinderUser.isLoggedIn = false;
+
+
+
+// // const regularUser = {
+// //     email: "some@google.com",
+// //     fullname: {
+// //         userFullName: {
+// //             firstName : "kaifuuu",
+// //             lastName : "mohd",
+// //             age: {
+// //                 umar: 21
+// //             }
+// //         }
+// //     }
 // // }
 
-
-// // singleton - we can generate the constructor objects
- 
-// const mySym = Symbol("key1")
-
-// const jsuser = {
-//     name: "kaifuuu",
-//     "fullname": "mohdkaifuuuu",
-//     [mySym]: "mykey1",
-//     age: 20,
-//     city: "Lucknow",
-//     email: "kaifuu@google.com",
-//     isLoggedIn: false,
-//     lastLoginDays: ["Monday", "Saturday"]
-// }
- 
-
-// console.log(jsuser);
-// console.log(jsuser.email);
-// console.log("diff approach");
-// console.log(jsuser["email"]);
+// // console.log(regularUser.fullname.userFullName.firstName);
 
 
-// console.log("checking the Symbol works here or not!");
-
-// console.log([mySym]);
-// console.log(typeof [mySym]);
-
-//  jsuser.greeting = function(){
-//     console.log("hello jsuser boobs");
-//  }
-
-//  jsuser.greetingTwo= function(){
-//     console.log(`hello jsuser ,${this.name}`);
-//  }
+// // console.log(regularUser.fullname.userFullName.lastName);
 
 
-//  console.log(jsuser.greeting());
-//   console.log(jsuser.greetingTwo());
+// // console.log(regularUser.fullname.userFullName.age);
 
 
 
-// Objects Part 2
+// // ************** OBejcts Part 2 **********
 
-const tinderUser = new Object() 
+// /*
+// const obj1 = {1: "a", 2: "b"};
+// const obj2 = {3: "c", 4: "d"};
+
+// // const obj3 = { obj1, obj2};
+// const obj3 = Object.assign({}, obj1, obj2)
 
 
-tinderUser.id = "123abc"
-tinderUser.name = "kaifuuu"
-tinderUser.isLoggedIn = false;
+// console.log(obj3);
+
+// */
+
+// const target = {a: "1", b: "2"};
+// const source = {b: "4", c: "5"};
+
+// const returnedTarget = Object.assign(target, source);
+
+// console.log(target);
+
+// console.log(returnedTarget == target);
+
+
+
+// console.log("Objects key, values, pair");
+
+// console.log(Object.keys(target));
+
+
+// console.log(Object.entries(tinderUser));
 
 
 
 
-console.log(tinderUser);
 
 
+
+
+
+// 
