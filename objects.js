6a8df2d8e3806f3c5c2b889125eaf30862e9b874
kaifyyy <mkaifm728@gmail.com>
2025-08-05 20:39:47 +0530

    more changes

--- objects.js
+++ objects.js
@@ -1,54 +1,59 @@
-// learning about Objects
+// // learning about Objects
 
 
-// function Person(name, age, city) {
-//     this.name = name;
-//     this.age = age;
-//     this.city = city;
+// // function Person(name, age, city) {
+// //     this.name = name;
+// //     this.age = age;
+// //     this.city = city;
 
+// // }
+
+
+// // singleton - we can generate the constructor objects
+ 
+// const mySym = Symbol("key1")
+
+// const jsuser = {
+//     name: "kaifuuu",
+//     "fullname": "mohdkaifuuuu",
+//     [mySym]: "mykey1",
+//     age: 20,
+//     city: "Lucknow",
+//     email: "kaifuu@google.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Saturday"]
 // }
-
-
-// singleton - we can generate the constructor objects
- 
-const mySym = Symbol("key1")
-
-const jsuser = {
-    name: "kaifuuu",
-    "fullname": "mohdkaifuuuu",
-    [mySym]: "mykey1",
-    age: 20,
-    city: "Lucknow",
-    email: "kaifuu@google.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
-}
  
 
-console.log(jsuser);
-console.log(jsuser.email);
-console.log("diff approach");
-console.log(jsuser["email"]);
+// console.log(jsuser);
+// console.log(jsuser.email);
+// console.log("diff approach");
+// console.log(jsuser["email"]);
 
 
-console.log("checking the Symbol works here or not!");
+// console.log("checking the Symbol works here or not!");
 
-console.log([mySym]);
-console.log(typeof [mySym]);
+// console.log([mySym]);
+// console.log(typeof [mySym]);
 
- jsuser.greeting = function(){
-    console.log("hello jsuser boobs");
- }
+//  jsuser.greeting = function(){
+//     console.log("hello jsuser boobs");
+//  }
 
- jsuser.greetingTwo= function(){
-    console.log(`hello jsuser ,${this.name}`);
- }
+//  jsuser.greetingTwo= function(){
+//     console.log(`hello jsuser ,${this.name}`);
+//  }
 
 
- console.log(jsuser.greeting());
-  console.log(jsuser.greetingTwo());
+//  console.log(jsuser.greeting());
+//   console.log(jsuser.greetingTwo());
 
 
 
 // Objects Part 2
 
+const tinderUser = new Object() 
+
+console.log(tinderUser);
+
+ 
