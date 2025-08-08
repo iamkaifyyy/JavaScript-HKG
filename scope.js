d9af03a595ef5a6e320d247406590b79f018d958
kaifyyy <mkaifm728@gmail.com>
2025-08-08 16:53:38 +0530

    more changes

--- scope.js
+++ scope.js
@@ -3,3 +3,23 @@
 // local or global Scope
 
 
+/*
+var c = 300;
+
+let a = 300;
+
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+*/
+
+// ***********Scope Level and Mini Hoisting **********
+
