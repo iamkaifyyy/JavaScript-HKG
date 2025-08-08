8cdbce3f6130223db1d042e928ec6b523e26b25b
kaifyyy <mkaifm728@gmail.com>
2025-08-08 14:47:48 +0530

    changes done here

--- functions.js
+++ functions.js
@@ -9,12 +9,18 @@ function sayMyName(){
 
 // sayMyName()
 
+//function addTwoNumbers(number1 , number2){
+    //console.log(number1 + number2);
+
+//}
+
 function addTwoNumbers(number1 , number2){
-    console.log(number1 + number2);
-
-
+    
+    let result = number1 + number2
+    return result;
 }
 
+const result = addTwoNumbers(2, null)
 
-addTwoNumbers(2, null)
+console.log("result: ", result);
 
