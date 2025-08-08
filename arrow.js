ca71b9fc436f25729013c055a7898934834aa10e
kaifyyy <mkaifm728@gmail.com>
2025-08-08 23:05:58 +0530

    changes : arrow function

--- arrow.js
+++ arrow.js
@@ -27,6 +27,8 @@ const user = {
 
 // ***** this keyword Code ***
 
+
+/*
 const person1 = {
     name: "Bond....JamesBond",
     favFood: "VodkaMartini",
@@ -41,4 +43,40 @@ const person1 = {
 }
 
 person1.sayHello();
-person1.eat
+person1.eat();
+
+*/
+
+// const chai = function(){
+//     let username = "kaifuuu",
+//     console.log(this.username);
+
+// }
+
+
+const chai = () => {
+    let username = "kaifuuu"
+    console.log(this);
+
+}
+
+chai();
+
+
+// ****** arrow function ******
+
+
+// const addTwo = (num1 + num2) => {
+//     return num1 + num2
+// }
+
+const addTwo = (num1, num2) => {
+    username: "kaifuuu"
+    // here we are returning the object in arrow function.
+
+    // for returning the object value we need to wrap the object in parenthesis , it's neccessary as fuckkk
+}
+
+
+
+console.log(addTwo(2, 3));
