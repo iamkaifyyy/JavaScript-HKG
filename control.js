58f4a9d7464b14dda09646d6224d5d91721c1020
kaifyyy <mkaifm728@gmail.com>
2025-08-09 16:46:27 +0530

    more commit on the way

---
+++ control.js
@@ -0,0 +1,20 @@
+// if
+
+const isUserloggedIn = true;
+ 
+
+
+if (isUserloggedIn) {
+    console.log("welcome to the website")
+}
+
+
+const score = 200;
+
+if(score > 100){
+    console.log("you are a pro gamer")
+}else if(score > 50){
+    console.log("you are a good gamer")
+}else{
+    console.log("you are a noob gamer")
+}                           
