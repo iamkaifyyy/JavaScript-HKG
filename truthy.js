37dd83d3074d72c442cc3e037ee3bada5b778d70
kaifyyy <mkaifm728@gmail.com>
2025-08-09 17:41:03 +0530

    more truthy falsy values

---
+++ truthy.js
@@ -0,0 +1,17 @@
+// **** truthy.js ******
+
+const userEmail = "kaifuuu@apple.com";
+
+if(userEmail){
+    console.log("Got User Email");
+} else {
+    console.log("Dont have email");
+}
+
+
+// This is a truthy value example for true or falsyy values
+
+// false, 0, -0, BigInt 0n, "", NaN, null, undefined are falsy values   
+
+// all other values are truthy values
+// for e.g. "0", "false", 
