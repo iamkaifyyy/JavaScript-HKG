20e6ed13f819574cd4951378f04b7492317dc2f4
kaifyyy <mkaifm728@gmail.com>
2025-08-09 23:45:09 +0530

    while loop

--- iterators.js
+++ iterators.js
@@ -21,7 +21,20 @@ for (let i = 0; i <= 10; i++) {
     console.log(`Innerloop : ${j} and inner loop is ${i}`);
 
     console.log(i + '*' + j + '='+ i * j);
-    
 
   }
 }
+
+// break and continue 
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5){
+        console.log(`Detected 5, ${index}`);
+        break;
+    }  
+    console.log(`value of i is ${index}`);
+
+}
+
+
+// continue - skip one time statement
