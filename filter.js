b1e34d021b4b81058d5cb8dfa393544168011d73
kaifyyy <mkaifm728@gmail.com>
2025-08-10 16:27:07 +0530

    more upcoming changes

---
+++ filter.js
@@ -0,0 +1,21 @@
+// for filter(), map(), reduce() 
+
+
+// const coding = ["js", "ruby", "python", "java", "cpp"];
+
+// const values = coding.forEach((item) => {
+//     console.log(item);
+//     return item;
+// });
+
+// console.log(values); // undefined, as forEach does not return anything
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];    
+
+// const newNums = numbers.filter((num) => num > 4)
+
+// console.log(newNums); // [5, 6, 7, 8, 9, 10]
+
+
+const myNums =
