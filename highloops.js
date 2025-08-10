76b5b7ebe18949a220dbe848a5db2d52bb7f46d0
kaifyyy <mkaifm728@gmail.com>
2025-08-10 12:58:43 +0530

    maps in JS

---
+++ highloops.js
@@ -0,0 +1,30 @@
+// for of loop 
+
+const arr = [1, 2, 3, 4, 5];
+
+// for (const i of arr) {
+//     console.log(i);
+// }
+
+
+// for string values
+
+// const greetings = "Hello World";
+
+// for (const greet of greetings){
+//     if (greet === " " ){
+//         console.log("Space also used");
+//     }
+//     console.log(`each character is ${greet}`);
+
+// }
+
+// Map 
+
+const map = new Map();
+
+map.set('In','India');
+map.set('US', 'United States');
+map.set('UK', 'United Kingdom');    
+
+console.log(map);
