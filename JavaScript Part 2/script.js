66f4e3b89d0d56ee9830e9aaf94e742d55dff462
kaifyyy <mkaifm728@gmail.com>
2025-08-13 14:25:46 +0530

    more Changes

--- JavaScript Part 2/script.js
+++ JavaScript Part 2/script.js
@@ -1,5 +1,10 @@
-let headings = document.getElementsByClassName('heading');
+// let headings = document.getElementsByClassName('heading');
 
-console.log(headings);
-console.dir(headings);
+// console.log(headings);
+// console.dir(headings);
 
+// console.log("printed");
+
+
+
+let paragraphs = document.getElementsByTagName('p');
