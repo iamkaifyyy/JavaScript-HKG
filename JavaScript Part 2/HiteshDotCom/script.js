7ffd36e34731a7267a8949051f3ca05a2f85b4af
kaifyyy <mkaifm728@gmail.com>
2025-08-19 00:13:14 +0530

    commit changes

--- JavaScript Part 2/HiteshDotCom/script.js
+++ JavaScript Part 2/HiteshDotCom/script.js
@@ -1,12 +1,16 @@
 // add Event Listener
 // This code listens for a click event on a button with the ID "btn" and alerts the user when the button is clicked.
 
-let document = document.getElementById("btn");
+button.addEventListener("click", () =>{
+    alert("Button clicked!");
+});
 
- document.addEventListener("click", () => {
-     alert("Button clicked!");
-     console.log("button was clicked");
- });
+button.addEventListener("click", ()=> {
+    document.querySelector(".box").innerHTML = "<b>Yayy you were clicked!</b> Enjoy your click"; 
+});
+
+
+
 
 // event object. 
 
