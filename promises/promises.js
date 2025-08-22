2a08478a33a694e74278694e13eaddc0bc385fa3
kaifyyy <mkaifm728@gmail.com>
2025-08-23 00:22:09 +0530

    initial commit

--- promises/promises.js
+++ promises/promises.js
@@ -1,3 +1,26 @@
 // ******* promises in JS *******
 
 console.log("promises");
+ 
+
+const promiseOne = new Promise(function(resolve, reject){
+    // do an async tasks, crytography
+    setTimeout(function(){
+        console.log('async task is completed');
+        resolve();
+
+    }, 1000);
+}) 
+
+promiseOne.then(function(){
+    console.log("promise consumed")
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("async tasks 2");
+        resolve();
+    }, 1000)
+}).then(function(){
+    console.log("async 2 resolved!")
+})
